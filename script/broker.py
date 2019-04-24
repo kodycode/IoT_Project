8e92026e9a430a0aab499c03e1fe1250bfff9145
Kodycode <kodythach@gmail.com>
2019-04-24 01:09:48 -0400

    Fully prepared timestamp/datetime upload

--- script/broker.py
+++ script/broker.py
@@ -1,10 +1,39 @@
 import paho.mqtt.client as mqtt
 import pyrebase
+import time
 
 
 def on_message(client, userdata, message):
-    print(message.topic + " " + str(message.payload))
+    global msg_sent
+    global db
+    global user
+    global pushing
+    global storage
+    if int(message.payload) == 1:
+        if not msg_sent and not pushing:
+            pushing = True
+            print('Uploading image..')
+            imageLocation = "images/{}.png".format(time.time())
+            storage.child(imageLocation).put("owo.png", user['idToken'])
+            imageURL = storage.child(imageLocation).get_url(user['idToken'])
+            data = {"imageUrl": "{}".format(imageURL), "TimeStamp": "{}".format(time.time())}
+            # Pass the user's idToken to the push method
+            print('Pushing..')
+            db.child("date").push(data, user['idToken'])
+            # send to firebase and wait until script goes back to 0
+            print('Data pushed')
+            msg_sent = True
+            pushing = False
+    else:
+        if not pushing:
+            if msg_sent:
+                # set script back to state where it can update firebase
+                msg_sent = False
+                print('Resetting..')
 
+
+msg_sent = False
+pushing = False
 
 config = {
   "apiKey": "apiKey",
@@ -14,11 +43,16 @@ config = {
 }
 
 firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
+user = auth.sign_in_with_email_and_password('test@gmail.com', '123456')
+db = firebase.database()
+storage = firebase.storage()
 
-broker_address = "10.0.0.7"
+broker_address = "10.0.0.82"
 client = mqtt.Client("python client")
 client.connect(broker_address)
 
 client.on_message = on_message
 client.subscribe("door")
+print("Broker online")
 client.loop_forever()
