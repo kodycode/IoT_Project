c81c5191887324e824ec23da68423e49d728ad24
Kody <kodythach@gmail.com>
2019-04-30 19:19:36 -0400

    Added push notifications

--- app/App.js
+++ app/App.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import firebase from 'firebase'
+import '@firebase/firestore'
 import { getStatusBarHeight } from 'react-native-status-bar-height'
-import { Dimensions, Image, ScrollView, StyleSheet, Text, View, TouchableOpacity, FlatList, Modal } from 'react-native'
+import { Platform, Dimensions, Image, ScrollView, StyleSheet, Text, View, TouchableOpacity, FlatList, Modal } from 'react-native'
+import { Permissions, Notifications } from 'expo';
 
 const { width, height } = Dimensions.get('screen')
 
@@ -11,25 +13,64 @@ export default class App extends React.Component {
     this.state = {
       currentImage: '',
       modalVisible: false,
-      dates: []
+      dates: [],
+      token: ''
     }
     console.disableYellowBox = true
   }
 
-  componentWillMount = () => {
-    this.initializeFirebase()
+  registerForPushNotificationsAsync = async () => {
+    const { status: existingStatus } = await Permissions.getAsync(
+      Permissions.NOTIFICATIONS
+    );
+    let finalStatus = existingStatus;
+  
+    // only ask if permissions have not already been determined, because
+    // iOS won't necessarily prompt the user a second time.
+    if (existingStatus !== 'granted') {
+      // Android remote notification permissions are granted during the app
+      // install, so this will only ask on iOS
+      const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS)
+      finalStatus = status
+    }
+    
+    // Stop here if the user did not grant permissions
+    if (finalStatus !== 'granted') {
+      return
+    }
+    
+    // Get the token that uniquely identifies this device
+    let token = await Notifications.getExpoPushTokenAsync()
+
+    if (Platform.OS === 'android') {
+      Expo.Notifications.createChannelAndroidAsync('door', {
+        name: 'door',
+        sound: true,
+      });
+    }
+  
+    // POST the token to your backend server from where you can retrieve it to send push notifications.
+    firebase.firestore().collection('notifications')
+    .doc('kodythach@gmail.com')
+    .set({ pushToken: token })
+    this.setState({ token })
+  }
+
+  componentWillMount = async () => {
+    await this.initializeFirebase()
     var dbInstance = firebase.database()
     var dateRef = dbInstance.ref('date')
+    await this.registerForPushNotificationsAsync()
     dateRef.on('value', (snapshot) => {
       var tempMessages = []
       for (date in snapshot.val()) {
         tempMessages.push(snapshot.val()[date])
       }
-      this.setState({ dates: tempMessages })
+      this.setState({ dates: tempMessages }, () => this.sendPushNotification())
     })
   }
 
-  initializeFirebase = () => {
+  initializeFirebase = async () => {
     // Initialize Firebase
     const firebaseConfig = {
       apiKey: '<YOUR-API-KEY>',
@@ -38,7 +79,24 @@ export default class App extends React.Component {
       storageBucket: '<YOUR-STORAGE-BUCKET>'
     }
 
-    firebase.initializeApp(firebaseConfig)
+    await firebase.initializeApp(firebaseConfig)
+  }
+
+  sendPushNotification = () => {
+    let response = fetch('https://exp.host/--/api/v2/push/send', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        to: this.state.token,
+        priority: 'high',
+        title: 'Door Sensor',
+        body: 'Door has opened',
+        channelId: 'door'
+      })
+    })
   }
 
   setModalVisible(visible, image='') {
